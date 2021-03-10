cafb52eee139db087bd13a7c1ecc2e77083b6272
WanCW <contact@wancw.idv.tw>
2021-03-11 03:15:18 +0800

    feat: make Telegram Bot can report account values

--- src/TelegramBot.ts
+++ src/TelegramBot.ts
@@ -3,7 +3,8 @@ import "./init" // this import is required
 import fetch from "node-fetch"
 import { Log } from "./Log"
 import { ServerProfile } from "./ServerProfile"
-import { Service } from "typedi"
+import Container, { Service } from "typedi"
+import { StatusReporter } from "./StatusReporter"
 
 interface TelegramUser {
     id: number
@@ -46,12 +47,13 @@ export class TelegramBot {
 
     async handleUpdate(update: TelegramUpdate): Promise<void> {
         const { message } = update
+
         if (!message) {
             this.log.jinfo({ event: "NotMessage" })
             return
         }
+        const { chat, text: msgText } = message
 
-        const { chat } = message
         if (chat.type !== "private") {
             this.log.jinfo({ event: "NotPrivateChat" })
             return
@@ -62,20 +64,49 @@ export class TelegramBot {
             return
         }
 
-        await this.replyTo(message, "Hi~ there", true)
+        if (!msgText) {
+            this.log.jinfo({ event: "NotTextMessage" })
+            return
+        }
+
+        switch (msgText.toLowerCase()) {
+            case "pnl":
+                const reporter = Container.get(StatusReporter)
+                const values = await reporter.getAccountValues()
+
+                const valStrs = [values.Perp, values.FTX, values.Perp.add(values.FTX)].map((b) => b.toFixed(20))
+                const maxValLen = Math.max(...valStrs.map((s) => s.length))
+                const fixedValStrs = valStrs.map((s) => s.padStart(maxValLen, " "))
+
+                await this.sendText(
+                    chat,
+                    [
+                        "```",
+                        `Perp:  ${fixedValStrs[0]}`,
+                        `FTX:   ${fixedValStrs[1]}`,
+                        `Total: ${fixedValStrs[2]}`,
+                        "```",
+                    ].join("\n"),
+                    true
+                )
+
+                // dirty hack: to release WebSocket connection
+                setTimeout(() => process.exit(0), 500)
+
+                break
+
+            default:
+                this.log.jinfo({ event: "UnrecognizedCommand", params: { text: msgText } })
+        }
     }
 
-    private async replyTo(message: TelegramMessage, text: string, disableNotification = false): Promise<void> {
+    private async sendText(chat: TelegramChat, text: string, disableNotification = false): Promise<void> {
         await this._sendMessage({
-            chat_id: message.chat.id,
-            reply_to_message_id: message.message_id,
+            chat_id: chat.id,
             text,
+            parse_mode: "MarkdownV2",
             disable_notification: disableNotification,
         })
-    }
-
-    private async sendText(chatId: number, text: string): Promise<void> {
-        await this._sendMessage({ chat_id: chatId, text })
     }
 
     private async _sendMessage(req: TelegramSendMessageRequest): Promise<void> {
@@ -91,6 +122,6 @@ export class TelegramBot {
 interface TelegramSendMessageRequest {
     chat_id: number
     text: string
+    parse_mode?: "MarkdownV2" | "HTML"
     disable_notification?: boolean
-    reply_to_message_id?: number
 }
